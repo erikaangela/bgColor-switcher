6e855d60b06ded1faeec2cf126bf4157e089f82e
Erika <erikaangela4@gmail.com>
2022-06-03 20:53:06 -0700

    File name change and added script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+
+document.getElementById("label").style.fontWeight = "900";
+
+function changeBackgroundColor() {
+  let color = document.getElementById("colors").value;
+  document.body.style.backgroundColor = color;
+}
